026b09af177dc1606f71cd3257622f3ec9597988
Abi Hunter <abihunter@gmail.com>
2018-05-11 14:23:16 +0100

    fixed static dates

--- function.js
+++ function.js
@@ -6,13 +6,13 @@ function randomDate(start, end) {
   return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
-let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const alertDay = randomDate(new Date(2018, 4, 9), new Date());
-const alertDateFormatted = days[alertDay.getDay()] + ", " + months[alertDay.getMonth()] + " " + alertDay.getDate() + ", " + "2018"
 
 
 function getAlert() {
+  let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let alertDay = randomDate(new Date(2018, 4, 9), new Date());
+  let alertDateFormatted = days[alertDay.getDay()] + ", " + months[alertDay.getMonth()] + " " + alertDay.getDate() + ", " + "2018"
   let randomHPAddress = () => Math.floor(Math.random() * (6100 - 5300) + 5300);
   let HPStreets = ["Kimbark", "Dorchester", "Harper", "Lake Park", "Ellis", "University", "Maryland", "Cottage Grove"];
   let weapons = ["sawed-off shotgun", "katana", "machete", "bludgeon", "mace", "flail", "ball-headed war club"]
@@ -33,6 +33,6 @@ function getAlert() {
   "The victim reported no physical injuries. The Four Horsemen of Chicago are investigating this crime.<br><br>" +
   "Be alert and aware of your surroundings at all times. Don't resist an attack by "  + theseSuspects + " unless absolutely necessary. \
   Avoid using cell phones or other electronics while patrolling the streets. When patrolling, try to patrol in pairs or groups." ;
-  return apocalypsewow;
+  return {alert: apocalypsewow, date: alertDateFormatted};
 
 }
